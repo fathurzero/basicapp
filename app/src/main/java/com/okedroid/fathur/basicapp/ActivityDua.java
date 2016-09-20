36cbe3582b854384f04d4785b9118265efe45a7a
fathurzero <ksatria.laptop@gmail.com>
2016-09-20 15:31:57 +0700

    Added 2 Activity

---
+++ app/src/main/java/com/okedroid/fathur/basicapp/ActivityDua.java
@@ -0,0 +1,13 @@
+package com.okedroid.fathur.basicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityDua extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dua);
+    }
+}
---
+++ app/src/main/java/com/okedroid/fathur/basicapp/ActivityTiga.java
@@ -0,0 +1,13 @@
+package com.okedroid.fathur.basicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityTiga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tiga);
+    }
+}
