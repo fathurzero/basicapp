eb42bb1404df18277a455f2d9351aff9a8d24170
fathurzero <ksatria.laptop@gmail.com>
2016-09-20 13:10:52 +0700

    Added Splashcreen Feature

---
+++ app/src/main/java/com/okedroid/fathur/basicapp/SplashScreen.java
@@ -0,0 +1,54 @@
+package com.okedroid.fathur.basicapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    private static int splashInterval = 2000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splashcreen);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                this.finish();
+            }
+
+
+
+
+            private void finish() {
+
+
+            }
+        }, splashInterval);
+
+    }
+
+    ;
+}
+
+
+
+
+
+
+
