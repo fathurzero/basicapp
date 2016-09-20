bd2d4c52df47fb8b167ffa1d0c66f8819dfdda0d
fathurzero <ksatria.laptop@gmail.com>
2016-09-20 21:43:45 +0700

    Insert Code Activity 3

--- app/src/main/java/com/okedroid/fathur/basicapp/ActivityTiga.java
+++ app/src/main/java/com/okedroid/fathur/basicapp/ActivityTiga.java
@@ -1,7 +1,9 @@
 package com.okedroid.fathur.basicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActivityTiga extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class ActivityTiga extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tiga);
 
+
     }
-}
+    public void pindahLayarKiri(View view) {
+        Intent i = new Intent(getApplicationContext(), ActivityDua.class);
+        startActivity(i);
+
+        overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
+
+
+    }
+    }
+
