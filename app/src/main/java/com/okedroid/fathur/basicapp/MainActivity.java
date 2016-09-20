54505981d345f4f76148e9958b8e17e82b12d42d
fathurzero <ksatria.laptop@gmail.com>
2016-09-20 19:42:28 +0700

    Insert Code add Button Activity 1

--- app/src/main/java/com/okedroid/fathur/basicapp/MainActivity.java
+++ app/src/main/java/com/okedroid/fathur/basicapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.okedroid.fathur.basicapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    //button pindah ke Activity 2
+
+    public void pindahLayar(View view) {
+        Intent i = new Intent(getApplicationContext(),ActivityDua.class);
+        startActivity(i);
+
     }
 }
