7a5e4d372c5ee1ed095c4b05ae4fbe461050bb28
fathurzero <ksatria.laptop@gmail.com>
2016-09-21 16:30:07 +0700

    Added fitur 2 back press Exit

--- app/src/main/java/com/okedroid/fathur/basicapp/ActivityTiga.java
+++ app/src/main/java/com/okedroid/fathur/basicapp/ActivityTiga.java
@@ -1,9 +1,11 @@
 package com.okedroid.fathur.basicapp;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class ActivityTiga extends AppCompatActivity {
 
@@ -19,8 +21,34 @@ public class ActivityTiga extends AppCompatActivity {
         startActivity(i);
 
         overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
-
-
-    }
     }
 
+
+        boolean doubleBackToExitPressedOnce = false;
+
+        @Override
+        public void onBackPressed() {
+            if (doubleBackToExitPressedOnce) {
+                //super.onBackPressed();
+
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//***Change Here***
+                startActivity(intent);
+                finish();
+                System.exit(0);
+                return;
+            }
+//fitur back dua kali keluar langsung ke home
+            this.doubleBackToExitPressedOnce = true;
+            Toast.makeText(this, "Tekan Back lagi Untuk Keluar", Toast.LENGTH_SHORT).show();
+
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    doubleBackToExitPressedOnce = false;
+                }
+            }, 2000);
+        }
+    }
--- app/src/main/java/com/okedroid/fathur/basicapp/MainActivity.java
+++ app/src/main/java/com/okedroid/fathur/basicapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.okedroid.fathur.basicapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +18,36 @@ public class MainActivity extends AppCompatActivity {
     //button pindah ke Activity 2
 
     public void pindahLayar(View view) {
-        Intent i = new Intent(getApplicationContext(),ActivityDua.class);
+        Intent i = new Intent(getApplicationContext(), ActivityDua.class);
         startActivity(i);
         overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
+    }
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            //super.onBackPressed();
+
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//***Change Here***
+            startActivity(intent);
+            finish();
+            System.exit(0);
+            return;
+        }
+//fitur back dua kali keluar langsung ke home
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
     }
 }
